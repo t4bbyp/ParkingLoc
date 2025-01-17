ab60b799e2a925dddaffb6082c66d051d592bead
t4bbyp <132542472+t4bbyp@users.noreply.github.com>
2025-01-17 17:47:12 +0200

    Create empty_location.php

---
+++ APIs/empty_location.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'db_connect.php'; // Include the database connection file
+
+$response = array();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $car_id = $_POST['car_id'];
+    
+    // Perform the delete operation in your database
+    $stmt = $conn->prepare("UPDATE cars SET car_location = '' WHERE car_id = :car_id");
+    $stmt->bindParam(':car_id', $car_id, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+        $response['error'] = false;
+        $response['message'] = 'Location emptied.';
+    } else {
+        $response['error'] = true;
+        $response['message'] = 'Error while emptying location.';
+    }
+} else {
+    $response['error'] = true;
+    $response['message'] = 'Invalid request.';
+}
+
+echo json_encode($response);
