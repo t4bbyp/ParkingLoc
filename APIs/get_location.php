5affa10cbfe0463403f5fe78d69d938188595e10
t4bbyp <132542472+t4bbyp@users.noreply.github.com>
2025-01-17 17:55:19 +0200

    Update get_location.php

--- APIs/get_location.php
+++ APIs/get_location.php
@@ -8,12 +8,10 @@ $response = array();
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     $user_id = $_POST['user_id'];
     
-    // Prepare and execute the SQL query
-$stmt = $conn->prepare("SELECT * FROM cars WHERE user_id = :user_id");
-$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-$stmt->execute();
-$location = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $conn->prepare("SELECT * FROM cars WHERE user_id = :user_id");
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $location = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-// Return contacts as JSON
 echo json_encode($location);
