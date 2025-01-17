4649893a26cc02051752977acbe1e9e2b533b9cb
t4bbyp <132542472+t4bbyp@users.noreply.github.com>
2025-01-17 17:54:42 +0200

    Update get_cars.php

--- APIs/get_cars.php
+++ APIs/get_cars.php
@@ -8,11 +8,10 @@ $response = array();
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     $user_id = $_POST['user_id'];
     
-    // Prepare and execute the SQL query
-$stmt = $conn->prepare("SELECT * FROM cars WHERE user_id = :user_id");
-$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-$stmt->execute();
-$cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $conn->prepare("SELECT * FROM cars WHERE user_id = :user_id");
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-// Return cars as JSON
+
 echo json_encode($cars);
